f2f4c6fb88defa026cbc9eb55a4d678bebb5f0d9
Gor MANUKYAN <gmanukya@e3r7p21.42.fr>
2018-03-21 16:12:17 +0100

    fixed array send

--- back/routes/login/login.js
+++ back/routes/login/login.js
@@ -10,7 +10,7 @@ router.post('/', function(req, res, next) {
 	const collection = db.collection('users');
 
 	if (!post.login || !post.password) {
-		res.status(400).json({fields: "Some informations are missing"});
+		res.status(400).json({fields: ["Some informations are missing"]});
 	}
 	else {
 		post.login = post.login.trim();
@@ -20,7 +20,7 @@ router.post('/', function(req, res, next) {
 			const mongoResult = result
 
 			if (result === null) {
-				res.status(400).json({login: "No user was found with that username"});
+				res.status(400).json({login: ["No user was found with that username"]});
 			}
 			else {
 				const password = result.password;
@@ -29,7 +29,7 @@ router.post('/', function(req, res, next) {
 					if (error) throw error;
 
 					if (result !== true) {
-						res.status(400).json({password: "The password is incorrect"});
+						res.status(400).json({password: ["The password is incorrect"]});
 					}
 					else {
 						// req.session.id = mongoResult._id;
