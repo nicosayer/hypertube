3ac4e10f3c599cbb057cc3dc47d1b75564fd955b
Gor MANUKYAN <gmanukya@e3r7p21.42.fr>
2018-03-21 15:21:18 +0100

    signup back and mongo

--- back/app.js
+++ back/app.js
@@ -26,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/signup', require('./routes/signup/signup'));
+app.use('/login', require('./routes/login/login'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
