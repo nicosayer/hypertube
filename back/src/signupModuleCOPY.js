fba58f21c653c037cd7389544d700c9ea62be5f2
Nicolas SAYER <nsayer@e3r7p21.42.fr>
2018-03-23 19:58:42 +0100

    reset pass

---
+++ back/src/signupModuleCOPY.js
@@ -0,0 +1,233 @@
+var express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+var mongo = require('../mongo');
+
+module.exports = function(req, post, isOAuth, callback) {
+	var error = [];
+	var db = mongo.getDb();
+	const collection = db.collection('users');
+
+
+	Object.keys(post).filter(function(key, index) {
+		if (key === 'password') {
+			return false;
+		}
+		return true;
+	}).map(function(key, index) {
+		if (key !== 'oauth') {
+			post[key] = post[key].trim();
+		}
+	});
+
+	post.firstName = post.firstName.toLowerCase().replace(/\s+/g, ' ');
+	post.lastName = post.lastName.toLowerCase().replace(/\s+/g, ' ');
+
+	if (post.email) {
+		post.email = post.email.toLowerCase();
+	}
+
+	if (!(post.firstName && post.lastName && post.login && post.email && post.password)) {
+		error.push('default');
+	}
+
+	if (post.login && post.login.length < 4) {
+		if (!error.includes('login')) {
+			error.push('login');
+		}
+	}
+
+	if (post.login && post.login.length > 20) {
+		if (!error.includes('login')) {
+			error.push('login');
+		}
+	}
+
+	if (post.email && post.email.length > 50) {
+		if (!error.includes('email')) {
+			error.push('email');
+		}
+	}
+
+	if (post.email && !(/^.+@.+\..+$/.test(post.email))) {
+		if (!error.includes('email')) {
+			error.push('email');
+		}
+	}
+
+
+	if (post.login && !(/^[a-zà-ÿ0-9]+$/i.test(post.login))) {
+		if (!error.includes('login')) {
+			error.push('login');
+		}
+	}
+
+	if (post.firstName && post.firstName.length < 1) {
+		if (!error.includes('firstName')) {
+			error.push('firstName');
+		}
+	}
+
+	if (post.firstName && post.firstName.length > 20) {
+		if (!error.includes('firstName')) {
+			error.push('firstName');
+		}
+	}
+
+	if (post.firstName && !(/^[a-zà-ÿ ]+$/i.test(post.firstName))) {
+		if (!error.includes('firstName')) {
+			error.push('firstName');
+		}
+	}
+
+
+	if (post.lastName && post.lastName.length < 1) {
+		if (!error.includes('lastName')) {
+			error.push('lastName');
+		}
+	}
+
+	if (post.lastName && post.lastName.length > 20) {
+		if (!error.includes('lastName')) {
+			error.push('lastName');
+		}
+	}
+
+	if (post.lastName && !(/^[a-zà-ÿ ]+$/i.test(post.lastName))) {
+		if (!error.includes('lastName')) {
+			error.push('lastName');
+		}
+	}
+
+
+	if (post.password && post.password.length < 6) {
+		if (!error.includes('password')) {
+			error.push('password');
+		}
+	}
+
+	if (post.password && post.password.length > 50) {
+		if (!error.includes('password')) {
+			error.push('password');
+		}
+	}
+
+	
+	if (!error.length) {
+		if(post.oauth) {
+			collection.findOne({oauth: post.oauth}, function (err, result) {
+				if (err) throw err;
+			});
+		}
+		else {
+			collection.findOne({ $or: [{email: post.email}, {login: {$regex: new RegExp('^' + post.login + '$', 'i')}}]}, function (err, result) {
+				if (err) throw err;
+				const userResult = result;
+				if (result !== null) {
+					if (!error.includes('email') && post.email === result.email) {
+						error.push('email');
+					}
+					else if (!error.includes('login') && post.login.toLowerCase() === result.login.toLowerCase()) {
+						error.push('login');
+					}
+					callback(error, 1);
+				}
+				else {
+					bcrypt.genSalt(10, function(err, salt) {
+						bcrypt.hash(post.password, salt, function(err, hash) {
+							post.password = hash;
+							collection.insert(post, function (err, result) {
+								if (err) throw err;
+								req.session._id = result.ops[0]._id;
+								callback(result.ops[0]);
+							});
+						});
+					});
+				}
+			});
+		}
+	}
+	else {
+		callback(error, 1)
+	}
+
+	collection.findOne({oauth: post.oauth}, function (err, result) {
+		if (err) throw err;
+
+		console.log(result);
+		if (!result) {
+			collection.findOne({email: post.email}, function (err, result) {
+				if (err) throw err;
+				const userResult = result;
+
+				new Promise((resolve, reject) => {
+					if (result !== null) {
+						if (!isOAuth) {
+							if (!error.includes('email')) {
+								error.push('email');
+							}
+							callback(error, 1);
+						}
+						else {
+							collection.update(
+								{email: post.email},
+								{$set: {oauth: post.oauth}}, function (err, result) {
+									if (err) throw err;
+
+									req.session._id = userResult._id;
+									callback(result);
+								});
+							}
+						}
+						else {
+							resolve(req.session._id);
+						}
+					})
+					.then(data => {
+						if (!isOAuth) {
+							collection.findOne({login: {$regex: new RegExp('^' + post.login + '$', 'i')}}, function (err, result) {
+								if (err) throw err;
+
+								if (result !== null) {
+									if (!error.includes('login')) {
+										error.push('login');
+									}
+								}
+
+								if (Object.keys(error).length === 0) {
+									bcrypt.genSalt(10, function(err, salt) {
+										bcrypt.hash(post.password, salt, function(err, hash) {
+											post.password = hash;
+											collection.insert(post, function (err, result) {
+												if (err) throw err;
+												console.log(result.ops[0]._id)
+												req.session._id = result.ops[0]._id;
+												callback(result.ops[0]);
+											});
+										});
+									});
+								}
+								else {
+									callback(error, 1)
+								}
+							});
+						}
+						else if (isOAuth && req.session && !data) {
+							collection.insert(post, function (err, result) {
+								if (err) throw err;
+								console.log(result.ops[0]._id)
+								req.session._id = result.ops[0]._id;
+								callback(result.ops[0]);
+							});
+						}
+					})
+				});
+			}
+			else {
+				console.log(result._id)
+				req.session._id = result._id;
+				callback(result);
+			}
+		});
+	}
