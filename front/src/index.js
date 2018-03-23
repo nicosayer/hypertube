65bbf03ef4c46970f9e6899a77ef1d00b8a56c64
Eric DE LA VARENDE <ede-la-v@e3r6p19.42.fr>
2018-03-23 11:21:48 +0100

    login auto with session

--- front/src/index.js
+++ front/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import configureStore from './configureStore'
 import registerServiceWorker from './registerServiceWorker';
+import { checkConnect } from './services/checkConnect'
 
 const store = configureStore()
-
+checkConnect()
 ReactDOM.render(
 		<App store={store}/>,
 	document.getElementById('root')
