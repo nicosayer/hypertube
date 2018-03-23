6bf04d21d04968beab154c5a3ba9c1342536c69f
Eric DE LA VARENDE <ede-la-v@e3r6p19.42.fr>
2018-03-23 12:00:38 +0100

    log auto

--- front/src/index.js
+++ front/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import configureStore from './configureStore'
 import registerServiceWorker from './registerServiceWorker';
-import { checkConnect } from './services/checkConnect'
+
+import { fetchWrap } from './services/fetchWrap'
+import { logMe } from './actions/me'
 
 const store = configureStore()
-checkConnect()
-ReactDOM.render(
-		<App store={store}/>,
-	document.getElementById('root')
-);
+fetchWrap('/isUserLoggedIn', {
+	method: 'GET',
+	credentials: 'include'
+})
+.then((payload) => {
+	//console.log(payload)
+	store.dispatch(logMe(payload))
+	ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+})
+.catch(() => {
+	ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+})
 registerServiceWorker();
