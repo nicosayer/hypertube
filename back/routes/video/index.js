35dd10bbf31747c2ea1aa8a7d6b5247f9e837c0d
Eric DE LA VARENDE <ede-la-v@e3r7p23.42.fr>
2018-03-25 14:32:22 +0200

    toujours sur la video

--- back/routes/video/index.js
+++ back/routes/video/index.js
@@ -1,31 +1,66 @@
-
-/*engine.on('download', function (piece){
-	console.log(piece)
-})
-
-engine.on('torrent', function (){
-	console.log("torrent")
-})*/
-
-
 var express = require('express');
-var torrentStream = require('torrent-stream');
-var ss = require('socket.io-stream');
-var socketStream = ss.createStream();
+//var torrentStream = require('torrent-stream');
+//var ss = require('socket.io-stream');
+//var socketStream = ss.createStream();
 var peerflix = require('peerflix')
+
+const TorrentSearchApi = require('torrent-search-api');
+ 
+const torrentSearch = new TorrentSearchApi();
+ 
+torrentSearch.enableProvider('1337x');
+torrentSearch.enableProvider('ThePirateBay');
+torrentSearch.enableProvider('Torrent9');
+torrentSearch.enableProvider('Torrentz2');
 
 exports = module.exports = function(io){
   io.sockets.on('connection', function (socket) {
-  	
-    socket.on('credentials', function (magnet) {
-    	console.log(magnet)
-    	var engine = peerflix(magnet)
-		console.log("test")
-		engine.server.on('listening', function() {
-		    var myLink = 'http://localhost:' + engine.server.address().port + '/';
-		    socket.emit('yooo', myLink)
-		});
-		/*engine.on('ready', function() {
+  var store;
+    socket.on('search', function (search) {
+    	torrentSearch.search(search, 'All', 10)
+	     .then(torrents => {
+	     	store = torrent
+	     	socket.emit('searchResult', torrents)
+	     	console.log(torrents)
+	     })
+	     .catch(err => {
+	         console.log(err);
+	     });
+    	
+		
+    	
+  	});
+
+  	socket.on('torrent', function (num) {
+    	torrentSearch.getMagnet(store[num])
+	    .then(magnet => {
+	    	var engine = peerflix(magnet)
+
+			engine.server.on('listening', function() {
+				engine.on('ready', function() {
+		    		console.log('readyyyyy')
+		    	})
+			    var myLink = 'http://localhost:' + engine.server.address().port + '/';
+				console.log(myLink)
+			    socket.emit('stream', myLink)
+			});
+	    })
+	    .catch(err => {
+	        console.log(err);
+	    });
+  	});
+
+    console.log('user connected')
+
+  	socket.on('disconnect', () => {
+	    console.log('user disconnected')
+	  });
+
+  });
+}
+
+
+/*engine.on('ready', function() {
 			console.log("readyyyyyyy")
 			engine.files.forEach(function(file) {
 				if (file.name == 'The.Big.Bang.Theory.S08E13.HDTV.x264-LOL.mp4')
@@ -52,14 +87,3 @@ exports = module.exports = function(io){
 				}
 			});
 		});*/
-    	
-  	});
-
-    console.log('user connected')
-
-  	socket.on('disconnect', () => {
-	    console.log('user disconnected')
-	  });
-
-  });
-}
