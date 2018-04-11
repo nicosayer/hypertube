e1092c3731e43bc81e22d0ac17b0a389856e16d1
Eric DE LA VARENDE <ede-la-v@e3r5p22.42.fr>
2018-04-11 14:56:21 +0200

    new subtitles

--- back/routes/home/video/findSubtitles.js
+++ back/routes/home/video/findSubtitles.js
@@ -23,7 +23,7 @@ router.get('/:magnet', function(req, res, next) {
 
 	engine.on('ready', function() {
 
-		console.log("readyyyyyyy")
+		console.log("readyyyyyyy subs")
 
 		var size = 0
 		var file;
@@ -34,12 +34,19 @@ router.get('/:magnet', function(req, res, next) {
 				file = fileTmp				
 			}
 		})
-		console.log(file.name)
+
 		var count = 0
 		var stream = file.createReadStream({start:0, end: 65536*3})
 		var stream2 = file.createReadStream({start: file.length - 65536*3, end: file.length - 1})
 		stream.on('end', () => {
 			console.log('end1')
+			count++
+			console.log(count)
+			if (count == 2)
+				hashAndDL(file, res)
+		})
+		stream2.on('end', () => {
+			console.log('end2')
 			count++
 			console.log(count)
 			if (count == 2)
@@ -51,13 +58,7 @@ router.get('/:magnet', function(req, res, next) {
 		stream2.on('data', () => {
 			console.log('data2')
 		})
-		stream2.on('end', () => {
-			console.log('end2')
-			count++
-			console.log(count)
-			if (count == 2)
-				hashAndDL(file, res)
-		})
+		
 		
 	})
 })
@@ -74,38 +75,41 @@ function hashAndDL(file, res) {
 			    }
 			})
 			.then((subs) => {
-				//console.log(subs)
-				subs = subs.body
-				//console.log(subs)
-				subs = JSON.parse(subs)
+				subs = JSON.parse(subs.body)
+				console.log(subs)
+				console.log(subs.length)
 				subs = subs.filter(sub => {
 					return sub.ISO639 == 'en'
 				})
-				//console.log(subs[0])
-				var buffer = []
-				console.log(subs[0].SubDownloadLink)
-				var streamsub = request(subs[0].SubDownloadLink)
-				streamsub.on('data', (data) => {
-					console.log(data)
-				})
-				var gunzip = zlib.createGunzip();
-				streamsub.pipe(gunzip).pipe(srt2vtt()).pipe(fs.createWriteStream('./public/subtitles/' + subs[0].MovieReleaseName + '.vtt'))
-				gunzip.on('data', function(data) {
-		            // decompression chunk ready, add it to the buffer
-		            buffer.push(data.toString())
+				if (subs.length != 0 && subs[0].SubDownloadLink) {
+					//console.log(subs)
+					console.log(subs[0])
+					var buffer = []
+					console.log(subs[0].SubDownloadLink)
+					var streamsub = request(subs[0].SubDownloadLink)
+					var gunzip = zlib.createGunzip();
+					streamsub.pipe(gunzip).pipe(srt2vtt()).pipe(fs.createWriteStream('./public/subtitles/' + subs[0].MovieReleaseName + '.vtt'))
+					gunzip.on('data', function(data) {
+			            // decompression chunk ready, add it to the buffer
+			            buffer.push(data.toString())
 
-		        }).on("end", function() {
-		        	console.log('res')
-		            // response and decompression complete, join the buffer and return
-		            buffer.join("");
-		            res.status(201).json({ sub: subs[0].MovieReleaseName + '.vtt' })
+			        }).on("end", function() {
+			        	console.log('res')
+			            // response and decompression complete, join the buffer and return
+			            buffer.join("");
+			            res.status(201).json({ sub: subs[0].MovieReleaseName + '.vtt' })
 
-		        }).on("error", function(e) {
-		            callback(e);
-		        })
+			        }).on("error", function(e) {
+			            callback(e);
+			        })
+		    	}
+		    	else {
+		    		res.status(300).json({message: 'no subtitles'})
+		    	}
 			})
+			.catch((error) => console.log(error))
 		})
-	}, 5000)
+	}, 1)
 }
 
 module.exports = router;
